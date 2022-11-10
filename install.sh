3c06ea2b15467e8b0c415224dda0059e3e19367b
vinstechmy <82468311+vinstechmy@users.noreply.github.com>
2022-11-10 23:10:20 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ if [[ -e /jffs/scripts/nat-start ]]; then
      mv /jffs/scripts/nat-start /jffs/scripts/nat-start.bak
 fi
 
-wget -O /jffs/scripts/nat-start scripts.advpn.xyz/asusttl65/nat-start
+wget -O /jffs/scripts/nat-start https://raw.githubusercontent.com/vinstechmy/Asus-Bypass-TTL/main/nat-start
 chmod +x /jffs/scripts/nat-start
 sh /jffs/scripts/nat-start
 nvram commit
